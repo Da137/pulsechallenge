c180412189af557fb922d38848a40bee8dc8c65e
David Attaie <Da137da@gmail.com>
2013-02-22 19:44:01 -0800

    cleaned up a little

--- PulseChallenge/PulseChallenge/ARSPCNewsObject.m
+++ PulseChallenge/PulseChallenge/ARSPCNewsObject.m
@@ -32,24 +32,4 @@
 	}
 	return self;
 }
-
--(void)parseDictionary:(NSDictionary *)preparsedDictionary
-{
-	[self setAuthor:[preparsedDictionary objectForKey:@"author"]];
-	[self setContent:[preparsedDictionary objectForKey:@"content"]];
-	[self setTitle:[preparsedDictionary objectForKey:@"title"]];
-	
-	//set up the URLS for the images this is a lower overhead than directly storing images
-	self.arrayOfMediaURLs = [[NSMutableArray alloc] init];
-	NSArray *mediaGroups = [preparsedDictionary objectForKey:@"mediaGroups"];
-	NSArray *contentArray = [[mediaGroups objectAtIndex:0] objectForKey:@"contents"];
-	
-	//setup the dictionary of media URLS
-	//Ideally we can use these for a slideshow of images later
-	for (NSDictionary *contentDictionary in contentArray) {
-		NSURL *urlOfImage = [NSURL URLWithString:[contentDictionary objectForKey:@"url"]];
-		[self.arrayOfMediaURLs addObject:urlOfImage];
-	}
-}
-
 @end
